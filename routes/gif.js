a56e998a18193003133f25fb7212d0ccbe65edd9
CYBER-TS <2401320048cj@cyber-u.ac.jp>
2025-06-07 18:52:31 +0900

    change message

--- routes/gif.js
+++ routes/gif.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 
-// 猫の画像を取得するルート
+// 犬の画像を取得するルート
 router.get('/', (req, res) => {
-    request('https://api.thecatapi.com/v1/images/search', function (error, response, body) {
+    request('https://dog.ceo/api/breeds/image/random', function (error, response, body) {
         if (!error && response.statusCode === 200) {
             const data = JSON.parse(body);
-            const catImageUrl = data[0].url; // 画像URLを取得
-            res.json({ imageUrl: catImageUrl }); // 画像URLをJSON形式で返す
+            const dogImageUrl = data.message; // 画像URLを取得
+            res.json({ imageUrl: dogImageUrl }); // 画像URLをJSON形式で返す
         } else {
-            res.status(500).json({ error: 'Failed to fetch cat image' }); // エラーハンドリング
+            res.status(500).json({ error: '犬の画像の取得に失敗しました' }); // エラーハンドリング
         }
     });
 });
